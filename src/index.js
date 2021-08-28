f03e9e347fbc79af5211fdf4a2cdaa570aa81df5
osagie_dot_developer <64429121+adesuwa-osagie@users.noreply.github.com>
2021-08-27 20:30:38 -0400

    Up to Moving the Ball

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Paddle from "/src/paddle";
 import InputHandler from "/src/input";
+import Ball from "/src/ball";
 
 let canvas = document.getElementById("gameScreen");
 let ctx = canvas.getContext("2d");
@@ -8,6 +9,7 @@ const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
 
 let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
+let ball = new Ball();
 
 /*A game loop is something that runs every frames, 
 updates all the objects, 
@@ -22,11 +24,13 @@ function gameLoop(timestamp) {
   let deltaTime = timestamp - lastTime;
   lastTime = timestamp;
 
-  ctx.clearRect(0, 0, 800, 600);
+  ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
   paddle.update(deltaTime);
   paddle.draw(ctx);
+
+  ball.draw(ctx);
 
   requestAnimationFrame(gameLoop);
 }
 
-gameLoop();
+requestAnimationFrame(gameLoop);
