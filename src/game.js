819210d48fab999422172cd88c2c0311db2778d8
osagie_dot_developer <64429121+adesuwa-osagie@users.noreply.github.com>
2021-08-28 14:34:02 -0400

    Up to loading levels

--- src/game.js
+++ src/game.js
@@ -1,6 +1,7 @@
 import Paddle from "/src/paddle";
 import InputHandler from "/src/input";
 import Ball from "/src/ball";
+import Trophy from "/src/trophy";
 
 export default class Game {
   constructor(gameWidth, gameHeight) {
@@ -12,7 +13,14 @@ export default class Game {
     this.ball = new Ball(this);
     this.paddle = new Paddle(this);
 
-    this.gameObjects = [this.ball, this.paddle];
+    let trophy = [];
+
+    for (let i = 0; i < 10; i++) {
+      trophy.push(new Trophy(this, { x: i * 52, y: 30 }));
+    }
+
+    //... => spread operator - adds an array to another array
+    this.gameObjects = [this.ball, this.paddle, ...trophy];
 
     new InputHandler(this.paddle);
   }
---
+++ src/trophy.js
@@ -0,0 +1,23 @@
+export default class Trophy {
+  constructor(game, position) {
+    this.image = document.getElementById("img_trophy");
+
+    this.game = game;
+
+    this.position = position;
+    this.width = 52;
+    this.height = 24;
+  }
+
+  update() {}
+
+  draw(ctx) {
+    ctx.drawImage(
+      this.image,
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.height
+    );
+  }
+}
