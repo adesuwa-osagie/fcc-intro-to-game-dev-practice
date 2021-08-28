84325182c92d57a99fd45a7d52f640778a2ead34
osagie_dot_developer <64429121+adesuwa-osagie@users.noreply.github.com>
2021-08-28 17:18:53 -0400

    Completed pause screen

--- src/game.js
+++ src/game.js
@@ -1,9 +1,16 @@
 import Paddle from "/src/paddle";
 import InputHandler from "/src/input";
 import Ball from "/src/ball";
-import Trophy from "/src/trophy";
+//import Trophy from "/src/trophy";
 
 import { buildLevel, level1 } from "/src/levels";
+
+const GAMESTATE = {
+  PAUSED: 0,
+  RUNNING: 1,
+  MENU: 2,
+  GAMEOVER: 3
+};
 
 export default class Game {
   constructor(gameWidth, gameHeight) {
@@ -12,6 +19,7 @@ export default class Game {
   }
 
   start() {
+    this.gamestate = GAMESTATE.RUNNING;
     this.ball = new Ball(this);
     this.paddle = new Paddle(this);
 
@@ -20,10 +28,12 @@ export default class Game {
     //... => spread operator - adds an array to another array
     this.gameObjects = [this.ball, this.paddle, ...trophies];
 
-    new InputHandler(this.paddle);
+    new InputHandler(this.paddle, this);
   }
 
   update(deltaTime) {
+    if (this.gamestate === GAMESTATE.PAUSED) return;
+
     this.gameObjects.forEach((object) => object.update(deltaTime));
 
     this.gameObjects = this.gameObjects.filter(
@@ -33,5 +43,24 @@ export default class Game {
 
   draw(ctx) {
     this.gameObjects.forEach((object) => object.draw(ctx));
+
+    if (this.gamestate === GAMESTATE.PAUSED) {
+      ctx.rect(0, 0, this.gameWidth, this.gameHeight);
+      ctx.fillStyle = "rgba(0, 0, 0, 0.5)";
+      ctx.fill();
+
+      ctx.font = "30px Arial";
+      ctx.fillStyle = "white";
+      ctx.textAlign = "center";
+      ctx.fillText("Paused", this.gameWidth / 2, this.gameHeight / 2);
+    }
+  }
+
+  togglePause() {
+    if (this.gamestate === GAMESTATE.PAUSED) {
+      this.gamestate = GAMESTATE.RUNNING;
+    } else {
+      this.gamestate = GAMESTATE.PAUSED;
+    }
   }
 }
